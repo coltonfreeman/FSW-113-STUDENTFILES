a02da65797ff32fc634e9df76b07df6a8b054a73
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2022-02-12 21:57:43 -0800

    update week3

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -1,15 +1,19 @@
 // declare any necessary variables
-let queryType;
-let itemID;
-
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
+let queryType;
+let itemID;
+
+
+
+
 function fetchData() {
     queryType = document.querySelector("#queryType").value;
     itemID = document.querySelector("#itemID").value;
-    getFromSWAPI();
-};
+    getFromSWAPI(queryType, itemID);
+}
+
 
 function getFromSWAPI() {
     // assign values to any necessary variables
@@ -24,9 +28,9 @@ function getFromSWAPI() {
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
 function updateInfo(data) {
-    document.querySelector("#dataLabel1").textContent = `Person Name:`;
-    document.querySelector("#dataValue1").textContent = `${data.name}`;
-    document.querySelector("#dataLabel2").textContent = `Hair Color:`;
-    document.querySelector("#dataValue2").textContent = `${data.hair_color}`;
-
+    const keys = Object.keys(data);
+    document.querySelector("#dataLabel1").innerText = keys[0];
+    document.querySelector("#dataValue1").innerText = data[keys[0]];
+    document.querySelector("#dataLabel2").innerText = keys[5];
+    document.querySelector("#dataValue2").innerText = data[keys[5]];
 };
